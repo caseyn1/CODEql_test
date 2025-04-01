fe6b48456c580f8b9994e5bd75724ba6b3cad3f6
Waterl00 <caseyjrnance@gmail.com>
2025-03-31 19:26:04 -0500

    Add vulnerable file

---
+++ vuln.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    name = request.args.get("name")
+    return f"<h1>Hello, {name}</h1>"  # 🔥 Reflected XSS
+
+if __name__ == "__main__":
+    app.run()
+
